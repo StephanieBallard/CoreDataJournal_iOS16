2ab4b8daac08ef718d3a5cf0f952fa7ebc7c3596
Stephanie Ballard <user.name>
2020-04-21 12:59:26 -0700

    added mood segmented control and outlet to class file

--- JournalCoreDataiOS16/JournalCoreDataiOS16/ViewControllers/CreateEntryViewController.swift
+++ JournalCoreDataiOS16/JournalCoreDataiOS16/ViewControllers/CreateEntryViewController.swift
@@ -12,6 +12,7 @@ class CreateEntryViewController: UIViewController {
 
     @IBOutlet weak var journalEntryTitle: UITextField!
     @IBOutlet weak var entryTextView: UITextView!
+    @IBOutlet weak var moodSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
